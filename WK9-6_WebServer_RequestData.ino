92cb8f2053c16d9a3d352782bf8d6a0a85b46ff9
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2021-11-16 12:00:20 +0800

    Add files via upload

---
+++ WK9-6_WebServer_RequestData.ino
@@ -0,0 +1,155 @@
+#include "WiFi.h"
+#include "WebServer.h"
+
+// SSID & Password
+const char* ssid = "tttddd";
+const char* password = "idkhowto";
+
+const int pin1 = 12;
+const int pin2 = 13;
+
+WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Try Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to your wi-fi modem
+  WiFi.begin(ssid, password);
+
+  // Check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+
+  //建立server的路徑
+  createWebServer();
+  
+  //server啟動
+  server.begin();
+  Serial.println("HTTP server started");
+  pinMode(pin1,OUTPUT);
+  ledcSetup(0, 5000, 10);
+  ledcAttachPin(pin2, 0);
+}
+
+void loop() {
+  server.handleClient();  //handleClient要寫在loop
+}
+
+
+//----設定HTML資料----
+String htmlContent = "\
+  <!DOCTYPE html>\
+   <html>\
+    <head>\
+      <style>\
+        html{ width:400;}\
+        body{ display: flex; flex-direction: column;}\
+         a { width: 200px; height: 50px; margin: 20px; font-size: 30pt; text-decoration: none;}\
+         #btn1 {background: #A1F255;}\
+         #btn2 {background: #F255A1;}\
+      </style>\
+    </head>\
+    <body>\
+      <a id='btn1' href='/led/on'>LED ON</a>\
+      <a id='btn2' href='/led/off'>LED OFF</a>\
+      <form method='get' action='/led/setLightBrightness'>\
+        <label>Led Brightness</label>\
+        <input type='number' name='lightBrightness'><input type='submit'>\
+      </form>\
+    </body>\
+  </html>";
+
+String LEDOpenPage = "\
+  <!DOCTYPE html>\
+   <html>\
+    <head>\
+      <style>\
+        html{ width:400;}\
+        body{ display: flex; flex-direction: column;}\
+         a { background: #CEF2F4; width: 200px; height: 50px; margin: 20px; font-size: 30pt; text-decoration: none;}\
+      </style>\
+    </head>\
+    <body>\
+      <h1>LED OPEN</h1>\
+      <a href='/'>Back</a>\
+    </body>\
+  </html>";
+
+String LEDCLOSEPage = "\
+  <!DOCTYPE html>\
+   <html>\
+    <head>\
+      <style>\
+        html{ width:400;}\
+        body{ display: flex; flex-direction: column;}\
+        a { background: #CEF2F4; width: 200px; height: 50px; margin: 20px; font-size: 30pt; text-decoration: none;}\
+      </style>\
+    </head>\
+    <body>\
+      <h1>LED CLOSE</h1>\
+      <a href='/'>Back</a>\
+    </body>\
+  </html>";
+  
+String response = "\
+  <!DOCTYPE html>\
+   <html>\
+    <head>\
+      <style>\
+        html{ width:400;}\
+        body{ display: flex; flex-direction: column;}\
+        a { background: #CEF2F4; width: 200px; height: 50px; margin: 20px; font-size: 30pt; text-decoration: none;}\
+      </style>\
+    </head>\
+    <body>\
+      <h1>";
+  
+//------設定路徑------
+void createWebServer()
+{
+  //初始頁面
+  server.on("/", []() {
+      server.send(200, "text/html", htmlContent);
+  });
+  server.on("/led/on", []() {
+    Serial.println("LED On");
+
+    //add your code here
+    digitalWrite(pin1,HIGH);
+    String response = "{\"LED On Success\"}";
+    server.send(200, "text/html", LEDOpenPage);
+  });
+  server.on("/led/off", []() {
+    Serial.println("LED Off");
+
+    //add your code here
+    digitalWrite(pin1,LOW);
+    String response = "{\"LED Off Success\"}";
+    server.send(200, "text/html", LEDCLOSEPage);
+  });
+  server.on("/led/setLightBrightness", []() {
+    //以server.arg&引數名稱，取得form傳來的資料
+    int lightBrightness = server.arg("lightBrightness").toInt();
+    //判斷條件    
+    if(lightBrightness>=0 && lightBrightness<=1024){
+      Serial.print("Set Brightness: ");
+      Serial.println(lightBrightness);
+
+      //add your code here
+      ledcWrite(0, lightBrightness);  
+      server.send(200, "text/html", response + "Set Brightness: " + (String)lightBrightness + "</h1><a href='/'>Back</a></body></html>");
+    } 
+    else {
+      String response = "{\"Error\":\"over range 0-1024\"}";
+      server.send(404, "application/json", response);
+    }
+  });
+}
